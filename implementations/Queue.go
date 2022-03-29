8b9eaf0f91876b9fd36502ea7e6c2e6456be944e
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-03-29 21:24:50 +0400

    Added Queue implementation

---
+++ implementations/Queue.go
@@ -0,0 +1,48 @@
+package implementations
+
+import (
+	"encoding/json"
+	"sync"
+
+	"github.com/Aranyak-Ghosh/golist/types"
+)
+
+type Queue[T any] struct {
+	data []T
+	mu   sync.Mutex
+}
+
+func (q *Queue[T]) UnmarshalJSON(data []byte) error {
+	var out []T
+
+	err := json.Unmarshal(data, &out)
+
+	if err != nil {
+		return types.UnmarshallError(err)
+	} else {
+		q.data = out
+		return nil
+	}
+}
+
+func (q *Queue[T]) Lenght() int {
+	return len(q.data)
+}
+
+func (q *Queue[T]) Enqueue(entry T) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	q.data = append(q.data, entry)
+}
+
+func (q *Queue[T]) Dequeue() (*T, bool) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	if len(q.data) == 0 {
+		return nil, false
+	} else {
+		var res = q.data[0]
+		q.data = q.data[1:]
+		return &res, true
+	}
+}
