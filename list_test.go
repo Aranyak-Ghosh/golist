52aa0f9bd9d2c17d28075700acd9604aceaac30d
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-04-03 18:32:16 +0400

    Added tests for map

--- list_test.go
+++ list_test.go
@@ -1,6 +1,7 @@
 package golist
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -64,6 +65,25 @@ func TestListFilter(t *testing.T) {
 	for i := range expected {
 		if (*q)[i] != expected[i] {
 			t.Errorf("Expected %d, got %d", expected[i], (*q)[i])
+			break
+		}
+	}
+}
+
+func TestListMap(t *testing.T) {
+	var q *List[int] = new(List[int])
+
+	*q = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	var expected = []string{"plsWork-1", "plsWork-2", "plsWork-3", "plsWork-4", "plsWork-5", "plsWork-6", "plsWork-7", "plsWork-8", "plsWork-9", "plsWork-10"}
+
+	res := (q.Map(func(x int) any {
+		return fmt.Sprintf("plsWork-%d", x)
+	})).(*List[any])
+
+	for i := range expected {
+		if (*res)[i] != expected[i] {
+			t.Errorf("Expected %s, got %s", expected[i], (*res)[i])
 			break
 		}
 	}
